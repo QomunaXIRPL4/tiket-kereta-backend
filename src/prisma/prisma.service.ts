d3916404d646a912a4e48d5435e7e917506a63fd
Qomuna <qomuna2008@gmail.com>
2026-05-21 22:47:41 +0700

    finalllllllll

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,13 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  kursi: any;
-    detailPembelian: any;
-    pelanggan: any;
-    jadwal: any;
-  $transaction(arg0: (tx: any) => Promise<{ tiket: any; details: any[]; }>) {
-      throw new Error('Method not implemented.');
-  }
   private readonly logger = new Logger(PrismaService.name);
 
   async onModuleInit() {
